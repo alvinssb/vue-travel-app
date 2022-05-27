d1646b2cb4af45eff9534d1a60f7dbaee0bb5427
Alvin Zheng <Alvin.Zheng@kla-tencor.com>
2022-05-27 20:58:41 +0800

    Add image assets & view page

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,9 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import BrazilView from "../views/BrazilView.vue";
+import HawaiiView from "../views/HawaiiView.vue";
+import JamaicaView from "../views/JamaicaView.vue";
+import PanamaView from "../views/PanamaView.vue";
 
 const routes = [
   {
@@ -15,6 +19,26 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+  },
+  {
+    path: "/brazil",
+    name: "brazil",
+    component: BrazilView,
+  },
+  {
+    path: "/hawaii",
+    name: "hawaii",
+    component: HawaiiView,
+  },
+  {
+    path: "/jamaica",
+    name: "jamaica",
+    component: JamaicaView,
+  },
+  {
+    path: "/panama",
+    name: "panama",
+    component: PanamaView,
   },
 ];
 
